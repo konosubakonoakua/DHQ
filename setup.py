0298d6fd1ac849ad2ae7ffc5e64a9da05a60db3c
konosubakonoakua <konosubaakua@163.com>
2021-04-27 20:41:25 +0800

    bump to 0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="DatasetsHelper", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="konosubakonoakua",
     author_email="ailike_meow@qq.com",
     description="A package to get normalization parameters used in different datasets like imagenet, cifar etc.",
