0c90df583f58e43f73ba7c54b3c6f9378812e004
konosubakonoakua <konosubaakua@163.com>
2021-04-27 17:03:13 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+# setuptools.setup()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DatasetsNormalizationParameters", # Replace with your own username
+    version="0.0.1",
+    author="konosubakonoakua",
+    author_email="ailike_meow@qq.com",
+    description="A package to get normalization parameters used in different datasets like imagenet, cifar etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/konosubakonoakua/DatasetsNormalizationParameters",
+    project_urls={
+        "Bug Tracker": "https://github.com/konosubakonoakua/DatasetsNormalizationParameters/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
