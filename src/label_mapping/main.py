e1b7087b545a96fef463653c752ef89e496d3e47
konosubakonoakua <konosubaakua@163.com>
2021-04-27 19:47:48 +0800

    删除旧文件

--- src/label_mapping/main.py
+++
@@ -1,5 +0,0 @@
-import json
-def get_imagenet_label_mapping() -> dict:
-    with open("./mapping.json", 'r') as f:
-        mapping = json.loads(f)
-    return mapping
--- src/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from ..label_mapping.main import get_imagenet_label_mapping
-from ..normlization.main import get_dataset_mean_std
